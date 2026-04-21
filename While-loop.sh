43997a94cf98834b394e31e79f0d7fd7949c5d29
Priyadas <mpriyadas4@gmail.com>
2026-04-21 14:26:47 +0530

    roboshop appl

---
+++ While-loop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#for loop examples
+# Iterating Over a Range of Numbers
+for i in {1..10}; do
+	echo "iterating $i"
+done
+
+# Iterating Over a List of Strings
+for fruit in apple banana orange; do
+	echo "I like $fruit"
+done
+
+#C-Style For Loop
+# Used for more complex numeric control with initialization, condition, and increment:
+for ((i = 0; i <= 10; i++)); do
+	echo "counter: $i"
+done
+
+# Iterating Over Files
+for file in ./*.sh; do
+	echo "processing $file"
+done
+
+# Single-Line Syntax
+for i in 1..10; do echo "iteration $i"; done
